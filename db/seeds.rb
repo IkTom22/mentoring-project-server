956394125e061f3b5262b8eaa2ef920a28eccf65
Iko <ikotominaga@gmail.com>
2023-12-11 12:08:10 +1300

    simple seed data is added

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,5 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+Service.create(name: 'Service 1')
+Service.create(name: 'Service 2')
